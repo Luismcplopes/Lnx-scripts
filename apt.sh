85dd90877f7b65f63fe6f86c21a7f4b727d5f778
Luismcplopes <luis.mcp.lopes@gmail.com>
2017-04-25 16:51:37 +0100

    Add files via upload

---
+++ apt.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+#chmod -x  *.sh
+echo= update e upgrade
+sudo apt-get update && sudo apt-get upgrade -y
+#exit
---
+++ myip.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+#chmod -x  *.sh
+ifconfig | grep addr:*
+#exit
