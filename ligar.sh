7d92f24ee18ed22425eaf44a0659d46aae93893b
Luismcplopes <luis.mcp.lopes@gmail.com>
2017-06-01 09:47:32 +0100

    Update ligar.sh

--- ligar.sh
+++ ligar.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-docker container attach 4f08ba4be37c
-#end
-
-teste
-pidof datahub.prediction.model.article.py
-ps -ef | grep -i datahub.prediction.model.article.py | awk {'print $2'} | xargs kill -9
-ps -aux | grep -i datahub.prediction.model.article.py | awk {'print $2'} | xargs kill -9
-
+#docker container attach 4f08ba4be37c
 
 ps -ef && read userInput 
 kill  $userInput;python datahub.prediction.model.article.py&
+#ps -ef | grep -i datahub.prediction.model.article.py | awk {'print $2'} | xargs kill -9
+#ps -aux | grep -i datahub.prediction.model.article.py | awk {'print $2'} | xargs kill -9
+#exit
+
+
+
