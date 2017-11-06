d0737730ec2641383cb95479850de9af00adb1c9
Luismcplopes <luis.mcp.lopes@gmail.com>
2017-11-06 13:53:39 +0000

    Update install-snmp.sh

--- install-snmp.sh
+++ install-snmp.sh
@@ -5,12 +5,12 @@ sudo apt-get install -y snmp snmpd &&
 sudo /etc/init.d/snmpd restart &&
 netstat -nl | grep -E "(local|161)" &&
 snmpwalk -v 2c -c  public localhost
-echo ################################################
-echo # notes:                                       #
-echo #      edit snmpd.conf in /etc/snmp/snmpd.conf #
-echo # you can change                               #
-echo #      port in agentAddress  udp:127.0.0.1:161 #
-echo #      change community in rocommunity public  #
-echo #      change Location in sysLocation          #
-echo #      change Contact in sysContact            #
-echo ################################################ 
+echo "################################################"
+echo "# notes:                                       #"
+echo "#      edit snmpd.conf in /etc/snmp/snmpd.conf #"
+echo "# you can change                               #"
+echo "#      port in agentAddress  udp:127.0.0.1:161 #"
+echo "#      change community in rocommunity public  #"
+echo "#      change Location in sysLocation          #"
+echo "#      change Contact in sysContact            #"
+echo "################################################"
