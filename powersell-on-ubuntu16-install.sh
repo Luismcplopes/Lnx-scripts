f721901c491da828c2bdd125c78cde94ef114dae
Luismcplopes <luis.mcp.lopes@gmail.com>
2017-12-04 17:39:05 +0000

    Create powersell-on-ubuntu16-install.sh

---
+++ powersell-on-ubuntu16-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Ubuntu 16.04 repository
+# Import the public repository GPG keys for Microsoft
+# Register the Microsoft Ubuntu 16.04 repository
+# Install powershell from Microsoft Repo
+sudo apt-get update && \
+sudo apt-get install -y --no-install-recommends apt-utils  ca-certificates curl apt-transport-https &&\
+rm -rf /var/lib/apt/lists/* 
+curl https://packages.microsoft.com/keys/microsoft.asc |sudo apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list &&\
+sudo apt-get update &&\
+sudo apt-get install -y --no-install-recommends powershell
