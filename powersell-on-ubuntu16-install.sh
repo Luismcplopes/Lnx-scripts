cc7bad4cc5518dd00d9fbfea11e59e9ec0e92be8
Luismcplopes <luis.mcp.lopes@gmail.com>
2017-12-04 22:41:53 +0000

    Update powersell-on-ubuntu16-install.sh

--- powersell-on-ubuntu16-install.sh
+++ powersell-on-ubuntu16-install.sh
@@ -9,4 +9,5 @@ rm -rf /var/lib/apt/lists/*
 curl https://packages.microsoft.com/keys/microsoft.asc |sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list &&\
 sudo apt-get update &&\
-sudo apt-get install -y --no-install-recommends powershell
+sudo apt-get install -y powershell
+# sudo apt-get install -y --no-install-recommends powershell
