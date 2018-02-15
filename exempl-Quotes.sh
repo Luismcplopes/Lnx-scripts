31b5c60daa8708e062c7ae324c802f3e40c29b43
Luismcplopes <luis.mcp.lopes@gmail.com>
2018-02-15 13:43:23 +0000

    Create exempl-Quotes.sh

---
+++ exempl-Quotes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+## Purpose: Examples for Quotes
+## Date: 20th Oct 2016
+## Author: Ankam Ravi Kumar
+## Start
+VAR=123
+TEST=Ravi
+## Double Quote example
+echo "Execute Double Quote $VAR"
+echo 'Executing Single Quote $VAR $TEST'
+echo "This Host Name: `hostname`"
+## END
