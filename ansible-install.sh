7397b337d91e2ed1732759487a3397aeefa4cee0
Luismcplopes <luis.mcp.lopes@gmail.com>
2018-03-21 14:09:13 +0000

    Update ansible-install.sh

--- ansible-install.sh
+++ ansible-install.sh
@@ -5,4 +5,5 @@ sudo apt-get install python-minimal aptitude -y  &&
 sudo apt-get update &&
 sudo apt-get install -y ansible  &&
 pip install pywinrm
+sudo apt-get clean && sudo apt-get autoremove && sudo apt-get autoclean 
 ansible --version
