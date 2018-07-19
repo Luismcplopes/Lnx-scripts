74ae193388abbcf24550f3dba6695defb594e242
Luis MCP Lopes <17655448+Luismcplopes@users.noreply.github.com>
2018-07-19 10:03:11 +0100

    Add files via upload

---
+++ myworkpc.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+sudo apt-get update && sudo apt-get upgrade -y &&\
+sudo apt-get instal -y tree wget w3m vim nano make git asciinema  &&\
+sudo curl -fsSL https://git.io/vFonT | sh  &&\
+sudo curl https://git.io/fNZZC | sh  &&\
+sudo apt-get clean && sudo apt-get autoremove && sudo apt-get autoclean 
